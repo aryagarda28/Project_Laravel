63dcad09c772858b7a3da65c5398976c758d335f
unknown <aryagarda11@gmail.com>
2021-12-08 22:59:36 +0700

    first commit

---
+++ example-app/resources/views/Fish variant.blade.php
@@ -0,0 +1,42 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="img/1.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">Kohaku Koi</h5>
+                <p class="card-text">Kohaku sebagai raja dari semua ikan koi merupakan varietas pertama ikan koi yang dikembangkan dengan dua warna yaitu putih (shiroji), dan bercak merah.</p>
+                <a href="#" class="btn btn-primary">BUY</a>
+    </div>
+</div>
+</div>
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="img/2.jpg" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Showa Sanshoku Koi</h5>
+            <p class="card-text">Showa Sanshoku atau yang popular dengan sebutan singkat Showa ini, adalah salah satu varitas Koi yang sangat digemari oleh para pecinta Koi di seluruh dunia. Dari namanya ‘Sanshoku’, yaitu ‘San’ yang berarti ‘tiga’ dan ‘Shoku’ artinya ‘warna’, maka varitas Showa ini cukup mudah dikenali</p>
+            <a href="#" class="btn btn-primary">BUY</a>
+    </div>
+    </div>
+</div> 
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="img/1.jpg" alt="Card image cap">
+    <div class="card-body">
+    <h5 class="card-title">Sanke Koi</h5>
+    <p class="card-text"> Koi Sanke adalah salah satu koiyang memiliki warna merah dan hitam dengan dasar putih dikenal dengan Taisho Sanshoku atau popular dengan sebutan singkat ‘Sanke’. Kata ‘Sanshoku’ dalam bahasa Jepang  artinya ‘3 warna’, dan ‘Taisho’ diambil dari nama Kaisar Jepang ketika Koi ini mulai popular yaitu pada Era Taisho (1912 – 1926). Tidak diketahui secara pasti bagaimana awal diketemukannya, namun diperkirakan pada pertengahan Era Meiji yaitu sekitar tahun 1900-an</p>
+    <a href="#" class="btn btn-primary">BUY</a>
+</div>
+    </div>
+    </div>
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="img/5.jpg" alt="Card image cap">
+    <div class="card-body">
+    <h5 class="card-title">Tancho Koi</h5>
+    <p class="card-text">Tancho adalah jenis ikan koi yang hanya memiliki warna Hi (merah) dan membentuk pola bulat (lingkaran) tepat di bagian kepala. Tancho yang bagus, tidak terdapat pola atau warna lain di badannya, walau hanya satu sisik saja.</p>
+    <a href="#" class="btn btn-primary">BUY</a>
+    </div>
+</div>
+    </div>
---
+++ example-app/resources/views/welcome.blade.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Arya Koi Farm</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">History</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Market Place 
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Fish Variant</a>
+          <a class="dropdown-item" href="#">Fish Feed</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">About</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="container">
+  <div class="content">
+      <h1>Koi Farm</h1>
+      <img src="img/background2.jpg" alt="" style="width:100%;">
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="img/1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Kohaku Koi</h5>
+    <p class="card-text">Kohaku sebagai raja dari semua ikan koi merupakan varietas pertama ikan koi yang dikembangkan dengan dua warna yaitu putih (shiroji), dan bercak merah.</p>
+    <a href="#" class="btn btn-primary">BUY</a>
+  </div>
+  </div>
+</div>
+    <div class="col">
+      <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="img/2.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Showa Sanshoku Koi</h5>
+        <p class="card-text">Showa Sanshoku atau yang popular dengan sebutan singkat Showa ini, adalah salah satu varitas Koi yang sangat digemari oleh para pecinta Koi di seluruh dunia. Dari namanya ‘Sanshoku’, yaitu ‘San’ yang berarti ‘tiga’ dan ‘Shoku’ artinya ‘warna’, maka varitas Showa ini cukup mudah dikenali</p>
+        <a href="#" class="btn btn-primary">BUY</a>
+    </div>
+    </div>
+  </div> 
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="img/1.jpg" alt="Card image cap">
+    <div class="card-body">
+    <h5 class="card-title">Sanke Koi</h5>
+    <p class="card-text"> Koi Sanke adalah salah satu koiyang memiliki warna merah dan hitam dengan dasar putih dikenal dengan Taisho Sanshoku atau popular dengan sebutan singkat ‘Sanke’. Kata ‘Sanshoku’ dalam bahasa Jepang  artinya ‘3 warna’, dan ‘Taisho’ diambil dari nama Kaisar Jepang ketika Koi ini mulai popular yaitu pada Era Taisho (1912 – 1926). Tidak diketahui secara pasti bagaimana awal diketemukannya, namun diperkirakan pada pertengahan Era Meiji yaitu sekitar tahun 1900-an</p>
+    <a href="#" class="btn btn-primary">BUY</a>
+  </div>
+  </div>
+</div>
+  <div class="col">
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="img/5.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Tancho Koi</h5>
+    <p class="card-text">Tancho adalah jenis ikan koi yang hanya memiliki warna Hi (merah) dan membentuk pola bulat (lingkaran) tepat di bagian kepala. Tancho yang bagus, tidak terdapat pola atau warna lain di badannya, walau hanya satu sisik saja.</p>
+    <a href="#" class="btn btn-primary">BUY</a>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+</div>
+  
+  </div>
+</div>
+
+  
+  </div>
+</div>
+
+</div>
+<div class="container">
+    <h2>About</h2>
+    <p>Ikan Koi adalah ikan yang berasal dari jepang,koi berasal dari bahasa Jepang yang berarti ikan karper  Di Jepang, koi menjadi semacam simbol cinta dan persahabatan,namun saat ini budidaya ikan koi bukan hanya ada di jepang namun ikan koi sudah dapat di budidayakan di indonesia oleh karna itu website ini dibuat untuk menjual atau membeli ikan maupun kebutuhan ikan dan juga untuk mengedukasi tentang ikan Koi Import Maupun Lokal
+    </p>
+    
+    <h2>Company Name</h3>
+    <p>Aryas Koi Farm Company<p>
+
+    <h2>Contact Us</h3>
+    <P>AryasKoi@net.com<p>
+    <p>0812-9835-8128<p>
+
+</div>
+</body>
+</html>
+
+
